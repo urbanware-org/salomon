97b0eee6e4f3f55f7ba07fe458e1aa6a3318f057
Ralf Kilian <urbanware-org@users.noreply.github.com>
2016-12-14 14:03:39 +0100

    Update colors.sh

--- core/colors.sh
+++ core/colors.sh
@@ -3,12 +3,12 @@
 # ============================================================================
 # Name:         Sane Log Monitor color management core script
 # Project:      SaLoMon
-# Copyright:    Copyright (C) 2015 by Ralf Kilian
+# Copyright:    Copyright (C) 2016 by Ralf Kilian
 # Website:      http://www.urbanware.org
 # ----------------------------------------------------------------------------
 # File:         colors.sh
-# Version:      1.6.2
-# Date:         2015-04-30
+# Version:      1.7.0
+# Date:         2016-12-14
 # Description:  Functions to read the color config files and to return the
 #               appropriate color code.
 # ----------------------------------------------------------------------------
