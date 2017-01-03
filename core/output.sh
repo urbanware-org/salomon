e542394d04c4a2e8f8e7a24c83cf750b958dbb33
Ralf Kilian <urbanware-org@users.noreply.github.com>
2017-01-03 19:02:57 +0100

    Update output.sh

--- core/output.sh
+++ core/output.sh
@@ -3,13 +3,13 @@
 # ============================================================================
 # Name:         Sane Log Monitor output core script
 # Project:      SaLoMon
-# Copyright:    Copyright (C) 2016 by Ralf Kilian
+# Copyright:    Copyright (C) 2017 by Ralf Kilian
 # Website:      http://www.urbanware.org
 # GitHub:       https://github.com/urbanware-org/salomon
 # ----------------------------------------------------------------------------
 # File:         output.sh
-# Version:      1.7.0
-# Date:         2016-12-14
+# Version:      1.7.1
+# Date:         2017-01-03
 # Description:  Functions to return output on the shell.
 # ----------------------------------------------------------------------------
 # Distributed under the MIT License:
