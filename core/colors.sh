a42a70886e9172fda4d0643c378cef19c76654e6
Ralf Kilian <urbanware-org@users.noreply.github.com>
2017-02-01 14:17:49 +0100

    Update colors.sh

--- core/colors.sh
+++ core/colors.sh
@@ -4,6 +4,7 @@
 # Name:         Sane Log Monitor color management core script
 # Project:      SaLoMon
 # Copyright:    Copyright (C) 2017 by Ralf Kilian
+# License:      Distributed under the MIT License
 # Website:      http://www.urbanware.org
 # GitHub:       https://github.com/urbanware-org/salomon
 # ----------------------------------------------------------------------------
@@ -12,26 +13,6 @@
 # Date:         2017-01-03
 # Description:  Functions to read the color config files and to return the
 #               appropriate color code.
-# ----------------------------------------------------------------------------
-# Distributed under the MIT License:
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
 # ============================================================================
 
 get_color_code() {
