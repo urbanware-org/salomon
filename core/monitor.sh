4ca9379707a65c4bb99e8c224f9306d6ff26c255
Ralf Kilian <urbanware-org@users.noreply.github.com>
2017-02-01 20:36:15 +0100

    Update monitor.sh

--- core/monitor.sh
+++ core/monitor.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 # ============================================================================
-# Name:         Sane Log Monitor file monitoring core script
 # Project:      SaLoMon
+# File:         File monitoring core script
 # Copyright:    Copyright (C) 2017 by Ralf Kilian
 # License:      Distributed under the MIT License
+# ----------------------------------------------------------------------------
 # Website:      http://www.urbanware.org
 # GitHub:       https://github.com/urbanware-org/salomon
-# ----------------------------------------------------------------------------
-# File:         monitor.sh
-# Version:      1.7.1
-# Date:         2017-01-03
-# Description:  Functions to monitor a file with SaLoMon.
 # ============================================================================
 
 monitor_input_file() {
