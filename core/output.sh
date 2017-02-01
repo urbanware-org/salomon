c236264494366d904964ffad714042144e987283
Ralf Kilian <urbanware-org@users.noreply.github.com>
2017-02-01 14:14:58 +0100

    Update output.sh

--- core/output.sh
+++ core/output.sh
@@ -4,6 +4,7 @@
 # Name:         Sane Log Monitor output core script
 # Project:      SaLoMon
 # Copyright:    Copyright (C) 2017 by Ralf Kilian
+# License:      Distributed under the MIT License
 # Website:      http://www.urbanware.org
 # GitHub:       https://github.com/urbanware-org/salomon
 # ----------------------------------------------------------------------------
@@ -11,26 +12,6 @@
 # Version:      1.7.1
 # Date:         2017-01-03
 # Description:  Functions to return output on the shell.
-# ----------------------------------------------------------------------------
-# Distributed under the MIT License:
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
 # ============================================================================
 
 print_line() {
