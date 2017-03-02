a651ceb08bf6b8edb75d575067c388b9c2337de3
Ralf Kilian <urbanware-org@users.noreply.github.com>
2017-03-02 18:04:23 +0100

    Update output.sh

--- core/output.sh
+++ core/output.sh
@@ -6,8 +6,8 @@
 # Copyright (C) 2017 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 # 
-# Website:  http://www.urbanware.org
-# GitHub:   https://github.com/urbanware-org/salomon
+# Website: http://www.urbanware.org
+# GitHub: https://github.com/urbanware-org/salomon
 # ============================================================================
 
 print_line() {
