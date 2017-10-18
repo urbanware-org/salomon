9a3c4d5fa398f1d77c49624481dd11ab9799d303
Ralf Kilian <github@urbanware.org>
2017-10-18 18:42:20 +0200

    Update confetti.sh

--- snippets/confetti.sh
+++ snippets/confetti.sh
@@ -4,11 +4,11 @@ confetti() {
     line_input="$1"
     line_output=""
 
-    if [ "$highlight" = "" ]; then
+    if [ -z "$highlight" ]; then
         highlight_random=1
     fi
 
-    for char in $(echo "$line_input" | sed -e "s/\(.\)/\1\n/g"); do
+    for char in $(sed -e "s/\(.\)/\1\n/g" <<< "$line_input"); do
         color_confetti=$(shuf -i 1-256 -n 1)
         if [ $highlight_random -eq 1 ]; then
             color_highlight=$(shuf -i 0-1 -n 1)
