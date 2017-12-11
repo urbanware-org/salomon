e008c1f1587ee37768e070170ed39b71c67d2b6a
urbanware-org <github@urbanware.org>
2017-12-11 18:32:19 +0100

    Update version number

--- core/global.sh
+++ core/global.sh
@@ -14,7 +14,7 @@ set_global_variables() {
     # Do not change any values below! See the 'salomon.cfg' file inside the
     # main directory of SaLoMon for configuration options.
 
-    version="1.9.2"
+    version="1.9.3"
 
     arg_case=""
     input_file=""
