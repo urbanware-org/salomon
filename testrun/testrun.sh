3ce51bab60e7b98de25d76aedfc738f98c1c07a8
urbanware-org <github@urbanware.org>
2019-05-04 16:36:37 +0200

    Revise testrun script

--- testrun/testrun.sh
+++ testrun/testrun.sh
@@ -5,96 +5,88 @@ salomon_script="$salomon_script_dir/salomon.sh"
 salomon_sample_log="$salomon_script_dir/samples/foobar.log"
 salomon_sample_colors="$salomon_script_dir/colors/log_colors.cfg"
 salomon_sample_log="$salomon_script_dir/samples/foobar.log"
+salomon_sample_log_temp="/tmp/salomon_foobar.log"
 salomon_args="-i $salomon_sample_log -c $salomon_sample_colors --analyze"
 salomon_status=0
+cp -f $salomon_sample_log $salomon_sample_log_temp
 
 echo
 echo "Starting automated tests. Please wait."
 echo
-sleep 3
+sleep 1
 
-$salomon_script --color-table
+clear
+$salomon_script $salomon_args -f "2014" -hm
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -f "2014" -hm --no-info
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -f "2014" -ha
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -f "2014" -ha --cut-off
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -f "config file" -hm -ic
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -f "config file" -hu -ic
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
-$salomon_script $salomon_args -e "config file"
+clear
+$salomon_script $salomon_args -f "config;file;success;foo" -hm -ic
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
-$salomon_script $salomon_args -r "2014-04-02"
+clear
+$salomon_script $salomon_args -e "process"
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
+$salomon_script $salomon_args -r "2014-04-02 "
+if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
+
+clear
 $salomon_script $salomon_args -h 4
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
+clear
 $salomon_script $salomon_args -t 4
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
-$salomon_script $salomon_args --slow --delay 10
-if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
-
-echo
-echo "Starting interactive test for monitoring ('--monitor')."
-echo
-echo "After the log lines have been printed, cancel the process using" \
-     "Ctrl+C and"
-echo "confirm the exit prompt."
-echo
-echo "Press the Return key to proceed."
-read
-
-$salomon_script --monitor -i $salomon_sample_log -c $salomon_sample_colors \
-                -f "2014" -hu -ic --prompt -t 12
-if [ $? -ne 2 ]; then salomon_status=$(( salomon_status + 1 )); fi
-
-echo
-echo "Starting interactive test for paused output ('--pause')."
-echo
-echo "After printing 4 lines there will be a pause which must be skipped" \
-     "manually."
-echo
-echo "Press the Return key to proceed."
-read
-
+clear
 $salomon_script -i $salomon_sample_log --analyze --pause 4
 if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
-echo
-echo "Starting interactive test for interactive dialogs ('--interactive')."
-echo
-echo "For this test, simply confirm each of the dialogs."
-echo
-echo "Press the Return key to proceed."
-read
+clear
+echo "==" >> $salomon_sample_log_temp
+echo "==  Press Ctrl+C now once to  cancel  this monitoring process." \
+     >> $salomon_sample_log_temp
+echo "==" >> $salomon_sample_log_temp
+$salomon_script --monitor -i $salomon_sample_log_temp \
+                -f "2014; cancel ;=" \
+                -c $salomon_sample_colors  -hm -ic --prompt -t 14
+if [ $? -ne 2 ]; then salomon_status=$(( salomon_status + 1 )); fi
 
-$salomon_script $salomon_args -f "2014" -r "-02" -hu -ic \
-                              --interactive
-if [ $? -ne 0 ]; then salomon_status=$(( salomon_status + 1 )); fi
-
-echo
+rm -f $salomon_sample_log_temp
+clear
+echo -e "\e[93m\c"
 echo ' _____         _                           _        _             '
 echo '|_   _|__  ___| |_ _ __ _   _ _ __     ___| |_ __ _| |_ _   _ ___ '
 echo '  | |/ _ \/ __| __| ´__| | | | ´_ \   / __| __/ _` | __| | | / __|'
 echo '  | |  __/\__ \ |_| |  | |_| | | | |  \__ \ || (_| | |_| |_| \__ \'
 echo '  |_|\___||___/\__|_|   \__,_|_| |_|  |___/\__\__,_|\__|\__,_|___/'
-echo
-echo "Testrun finished."
+echo -e "\e[0m"
+echo "  Testrun finished."
 if [ $salomon_status -eq 0 ]; then
-    echo "Overall status: Successfully passed"
+    echo -e "  Overall status: \e[92mSuccessfully passed\e[0m"
 else
-    echo "Overall status: Failed (at least one test)"
+    echo -e "  Overall status: \e[91mFailed\e[0m (at least one test)"
 fi
 echo
+exit $salomon_status
