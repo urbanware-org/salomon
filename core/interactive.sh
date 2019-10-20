32e0cb6453a6a5242d96678c64df9356b8e78cab
urbanware-org <github@urbanware.org>
2019-10-20 22:11:28 +0200

    Add new core module

---
+++ core/interactive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ============================================================================
+# SaLoMon - Simple log file monitor and analyzer
+# Interactive dialog mode core script
+# Copyright (C) 2019 by Ralf Kilian
+# Distributed under the MIT License (https://opensource.org/licenses/MIT)
+#
+# GitHub: https://github.com/urbanware-org/salomon
+# GitLab: https://gitlab.com/urbanware-org/salomon
+# ============================================================================
