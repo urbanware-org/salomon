cd9b8d38be2c1668d7610837a25082b6e86efb32
urbanware-org <github@urbanware.org>
2019-10-24 19:59:27 +0200

    Add script header

--- testrun/testrun.sh
+++ testrun/testrun.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# ============================================================================
+# SaLoMon - Simple log file monitor and analyzer
+# Simple testrun script
+# Copyright (C) 2019 by Ralf Kilian
+# Distributed under the MIT License (https://opensource.org/licenses/MIT)
+#
+# GitHub: https://github.com/urbanware-org/salomon
+# GitLab: https://gitlab.com/urbanware-org/salomon
+# ============================================================================
+
 salomon_script_dir=$(dirname $(readlink -f $0) | sed -e "s/testrun//g")
 salomon_script="$salomon_script_dir/salomon.sh"
 salomon_testrun_log="/tmp/salomon_testrun_$$.log"
@@ -100,3 +110,5 @@ echo
 echo -e "For details see the file \e[96m$salomon_testrun_log\e[0m."
 echo
 exit $salomon_status
+
+# EOF
