8867e5a4869fb44e5de6f435726fbebedfade4e4
urbanware-org <github@urbanware.org>
2019-12-07 15:00:39 +0100

    Raise version number

--- core/global.sh
+++ core/global.sh
@@ -16,7 +16,7 @@ set_global_variables() {
     # the main directory of SaLoMon for configuration options.
     # ------------------------------------------------------------------------
 
-    version="1.15.1"
+    version="1.16.0"
 
     arg_case=""
     arg_list=$(readlink -f "$0")
