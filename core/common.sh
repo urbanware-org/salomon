7d2650315676b5f38e379381dfa61dea040d2503
urbanware-org <github@urbanware.org>
2020-09-23 20:51:24 +0200

    Add countdown for delay

--- core/common.sh
+++ core/common.sh
@@ -269,7 +269,15 @@ deprecated_argument() {
             "${cl_lr}removed${cl_n}"
     echo -e "        somewhen. You should use '${cl_lc}${arg_i}${cl_n}'" \
             "instead."
-    sleep 3
+echo
+    wait_delay=3
+    for sec in $(seq 1 $wait_delay); do
+        echo -e "        Proceeding in $wait_delay seconds.      \r\c"
+        sleep 1
+        wait_delay=$(( wait_delay - 1 ))
+    done
+        echo "        Proceeding.                                 "
+
 }
 
 prepare_path() {
