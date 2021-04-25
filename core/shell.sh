7b846280ebceb22161221bae18420bad0d146e0a
urbanware-org <github@urbanware.org>
2021-04-25 22:31:15 +0200

    Revise output messages

--- core/shell.sh
+++ core/shell.sh
@@ -13,9 +13,11 @@ shell_precheck() {
     if [ "$?" != "0" ]; then
         cat <<- end
 
-Salomon cannot be started because the required Bash shell does not seem to be
-installed. For Salomon, the Bash shell is a mandatory dependency and it will
-not work without it.
+Salomon cannot be run.
+
+The reason is that the required Bash shell does not seem to be installed. For
+Salomon, the Bash shell is a mandatory dependency and so it will not work
+without it.
 
 No matter which shell you are using, the Bash shell (version 4 or higher) must
 be installed in order to use Salomon as it takes advantage of certain features
@@ -26,9 +28,12 @@ end
     elif [ ! -n "$BASH" ]; then
         cat <<- end
 
-Salomon must be executed using the Bash shell and has determined that it was
-with a different one (maybe explicitly) which is not supported. Please run the
-compatibility check script ('compat.sh') for details.
+Salomon cannot be run.
+
+All scripts must be executed using the required Bash shell and this one has
+determined that it was executed with a different one (maybe explicitly) which
+is not supported. Please run the compatibility check script ('compat.sh') for
+details.
 
 end
         exit 1
@@ -38,9 +43,11 @@ end
         if [ $bash_major -lt 4 ]; then
             cat <<- end
 
-Salomon requires at least version 4 of the Bash shell. The currently installed
-version is $bash_version which is not supported. Please run the compatibility
-check script ('compat.sh') for details.
+Salomon cannot be run.
+
+The reason is that it requires at least version 4 of the Bash shell. The
+currently installed version is $bash_version which is not supported. Please
+run the compatibility check script ('compat.sh') for details.
 
 end
             exit 1
@@ -53,9 +60,11 @@ shell_precheck_compat() {
     if [ "$?" != "0" ]; then
         cat <<- end
 
-Salomon cannot be started because the required Bash shell does not seem to be
-installed. For Salomon, the Bash shell is a mandatory dependency and it will
-not work without it.
+Salomon cannot be run.
+
+The reason is that the required Bash shell does not seem to be installed. For
+Salomon, the Bash shell is a mandatory dependency and so it will not work
+without it.
 
 No matter which shell you are using, the Bash shell (version 4 or higher) must
 be installed in order to use Salomon as it takes advantage of certain features
@@ -66,9 +75,12 @@ end
     elif [ ! -n "$BASH" ]; then
         cat <<- end
 
-Salomon must be executed using the Bash shell and has determined that it was
-with a different one (maybe explicitly) which is not supported. Please run the
-compatibility check script ('compat.sh') for details.
+Salomon cannot be run.
+
+All scripts must be executed using the required Bash shell and this one has
+determined that it was executed with a different one (maybe explicitly) which
+is not supported. Please run the compatibility check script ('compat.sh') for
+details.
 
 end
         exit 1
