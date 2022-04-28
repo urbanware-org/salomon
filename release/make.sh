eb95b548a5b2b749212eeb149def0552b41ab373
urbanware-org <github@urbanware.org>
2022-04-28 21:57:19 +0200

    Update copyright information

--- release/make.sh
+++ release/make.sh
@@ -3,7 +3,7 @@
 #
 # Salomon - Simple log file monitor and analyzer
 # Release builder script
-# Copyright (c) 2021 by Ralf Kilian
+# Copyright (c) 2022 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
 # GitHub: https://github.com/urbanware-org/salomon
