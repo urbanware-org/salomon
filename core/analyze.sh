3d9c2ecc3461c0b21f6051927357c83d2ba868b4
urbanware-org <github@urbanware.org>
2023-06-16 09:43:48 +0200

    Adjust command-line arguments

--- core/analyze.sh
+++ core/analyze.sh
@@ -51,7 +51,7 @@ analyze_input_file() {
     fi
 
     if [ -n "$egrep_pattern" ]; then
-        grep -E -i $(sed -e "s/^|//g" <<< $egrep_pattern) $temp_file \
+        grep -Ei $(sed -e "s/^|//g" <<< $egrep_pattern) $temp_file \
               > ${temp_file}.presort
         mv -f ${temp_file}.presort $temp_file
     fi
