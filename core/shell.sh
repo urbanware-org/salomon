a4fc3b72f5491ac7c5d7ba2df7f1ebf5f183f1e5
urbanware-org <github@urbanware.org>
2026-02-01 10:23:48 +0100

    Remove unnecessary checks

--- core/shell.sh
+++ core/shell.sh
@@ -9,51 +9,20 @@
 #
 
 shell_precheck() {
-    command -v bash >/dev/null 2>&1
-    if [ "$?" != "0" ]; then
+    bash_version=$(sed -e "s/-.*//g" <<< $BASH_VERSION)
+    bash_major=$(sed -e "s/\..*//g" <<< $bash_version)
+    if [ $bash_major -lt 4 ]; then
         cat <<- end
 
 Salomon cannot be run.
 
-The reason for this is that the Bash shell does not seem to be installed. For
-Salomon, the Bash shell is a mandatory dependency and so it will not work
-without it.
-
-No matter which shell you are using, the Bash shell (version 4 or higher) must
-be installed in order to use Salomon as it takes advantage of certain features
-provided by the Bash shell.
-
-end
-        exit 1
-    elif [ ! -n "$BASH" ]; then
-        cat <<- end
-
-Salomon cannot be run.
-
-The reason for this is that all of its components require the Bash shell in
-order to run and this script has determined that it was (maybe explicitly)
-executed with a different one which is not supported.
-
-You may run the compatibility check script ('compat.sh') for details.
-
-end
-        exit 1
-    else
-        bash_version=$(sed -e "s/-.*//g" <<< $BASH_VERSION)
-        bash_major=$(sed -e "s/\..*//g" <<< $bash_version)
-        if [ $bash_major -lt 4 ]; then
-            cat <<- end
-
-Salomon cannot be run.
-
-The reason for this is that it requires at least version 4 of the Bash shell
+The reason for this is that it requires at least version 4.3 of the Bash shell
 and the currently installed version is $bash_version which is not supported.
 
 You may run the compatibility check script ('compat.sh') for details.
 
 end
             exit 1
-        fi
     fi
 }
 
